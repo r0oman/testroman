35aeb859f9c4b7ea959ca8b4b51106fd2c625a0f
r0oman <34817046+r0oman@users.noreply.github.com>
2018-05-01 21:16:57 +0400

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 8080
-
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("Server -",PORT)
-httpd.serve_forever()
