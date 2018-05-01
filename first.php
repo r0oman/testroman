2f124ea93bbf4083116f2930d326427a8e077f76
r0oman <34817046+r0oman@users.noreply.github.com>
2018-05-01 21:18:26 +0400

    Delete first.php

--- first.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<link rel="icon" href="Images/Games-icon.png" type="image">
-	<link rel="stylesheet" type="text/css" href="Css/css.css">
-	<title>My second Site</title>
-</head>
-<meta charset="UTF-8">
-<body>
